21f19cf2f369f10f27c60d9177c9f0dccc581207
seelife <kmsims2@gmail.com>
2016-09-07 20:38:37 -0500

    initial testing

--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,7 @@ Rails.application.routes.draw do
   end
 
   root 'welcome#index'
+
+  get '/home', to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
